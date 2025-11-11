9547cd9fa01fbba3b17292045c5a2cb5f2fc6568
Beatrice Wambui <bettyneema2@gmail.com>
2025-11-11 17:36:49 +0300

    Introduce db_config.py to centralize and export SQLAlchemy instance

---
+++ server/db_config.py
@@ -0,0 +1,15 @@
+# server/db_config.py
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+# Naming convention for foreign keys and indices (same as in app.py)
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
