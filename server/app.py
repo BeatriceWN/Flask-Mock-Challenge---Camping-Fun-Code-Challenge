3e6790fad073e6d0ff611a596d1e990a7aabe9e4
Beatrice Wambui <bettyneema2@gmail.com>
2025-11-11 16:25:06 +0300

    Update app.py setup and database configuration

--- server/app.py
+++ server/app.py
@@ -5,3 +5,11 @@ from flask_migrate import Migrate
 from sqlalchemy import MetaData
 from server.config import Config
 
+# Naming convention for foreign keys and indices
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
