a28dd630af73970c3485f018084fb20df658b642
Beatrice Wambui <bettyneema2@gmail.com>
2025-11-11 17:37:30 +0300

    Update app.py to use db.init_app() from db_config for better structure

--- server/app.py
+++ server/app.py
@@ -1,26 +1,15 @@
 import os
 from flask import Flask, make_response, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import MetaData
 from server.config import Config
-
-# Naming convention for foreign keys and indices
-convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
+from server.db_config import db, metadata
 
 # Initialize Flask App
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Initialize SQLAlchemy with naming convention
-metadata = MetaData(naming_convention=convention)
-db = SQLAlchemy(app, metadata=metadata)
+# Initialize SQLAlchemy with the Flask App
+db.init_app(app) # <-- NEW WAY TO INITIALIZE DB
 
 # Initialize Flask-Migrate
 migrate = Migrate(app, db)
@@ -39,6 +28,7 @@ def not_found(error):
 @app.errorhandler(405)
 def method_not_allowed(error):
     return make_response(jsonify({'error': 'Method Not Allowed'}), 405)
+
 
 if __name__ == '__main__':
     # Add context for flask commands
